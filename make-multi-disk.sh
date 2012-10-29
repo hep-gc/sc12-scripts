0422f6701eff06dafab8fe93059b2a4c268c1947
Ian Gable <igable@uvic.ca>
2012-10-28 21:26:54 -0700

    make file system and mount

--- make-multi-disk.sh
+++ make-multi-disk.sh
@@ -8,3 +8,21 @@ MegaCli -CfgLdAdd -r0 [252:4] WT NORA DIRECT -strpsz 1024 -a0
 MegaCli -CfgLdAdd -r0 [252:5] WT NORA DIRECT -strpsz 1024 -a0
 MegaCli -CfgLdAdd -r0 [252:6] WT NORA DIRECT -strpsz 1024 -a0
 MegaCli -CfgLdAdd -r0 [252:7] WT NORA DIRECT -strpsz 1024 -a0
+
+
+mkfs.xfs -f -d sunit=2048,swidth=2048 /dev/sdb
+mkfs.xfs -f -d sunit=2048,swidth=2048 /dev/sdc
+mkfs.xfs -f -d sunit=2048,swidth=2048 /dev/sdd
+mkfs.xfs -f -d sunit=2048,swidth=2048 /dev/sde
+mkfs.xfs -f -d sunit=2048,swidth=2048 /dev/sdf
+mkfs.xfs -f -d sunit=2048,swidth=2048 /dev/sdg
+mkfs.xfs -f -d sunit=2048,swidth=2048 /dev/sdh
+
+
+mount -t xfs /dev/sdb /ssd1
+mount -t xfs /dev/sdc /ssd2
+mount -t xfs /dev/sdd /ssd3
+mount -t xfs /dev/sde /ssd4
+mount -t xfs /dev/sdf /ssd5
+mount -t xfs /dev/sdg /ssd6
+mount -t xfs /dev/sdh /ssd7
