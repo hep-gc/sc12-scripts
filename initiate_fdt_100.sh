9e7903fdf15e64f1132f0aee76818b7b76ebd92e
Ian Gable <igable@uvic.ca>
2012-11-01 20:29:20 -0700

    push to a single server

--- initiate_fdt_100.sh
+++ initiate_fdt_100.sh
@@ -8,10 +8,10 @@ fdtClient -c 10.20.3.101 -p 54305 -d /bulk/ssd5/  /bulk/ssd5/100Gfile.dat &> /ro
 fdtClient -c 10.20.3.101 -p 54306 -d /bulk/ssd6/  /bulk/ssd6/100Gfile.dat &> /root/log/fdt6.log &
 fdtClient -c 10.20.3.101 -p 54307 -d /bulk/ssd7/  /bulk/ssd7/100Gfile.dat &> /root/log/fdt7.log &
 fdtClient -c 10.20.3.101 -p 54308 -d /bulk/ssd8/  /bulk/ssd8/100Gfile.dat &> /root/log/fdt8.log &
-#fdtClient -c 10.20.3.101 -p 54309 -d /bulk/ssd9/  /bulk/ssd9/100Gfile.dat &> /root/log/fdt9.log &
-#fdtClient -c 10.20.3.101 -p 54310 -d /bulk/ssd10/  /bulk/ssd10/100Gfile.dat &> /root/log/fdt10.log &
-#fdtClient -c 10.20.3.101 -p 54311 -d /bulk/ssd11/  /bulk/ssd11/100Gfile.dat &> /root/log/fdt11.log &
-#fdtClient -c 10.20.3.101 -p 54312 -d /bulk/ssd12/  /bulk/ssd12/100Gfile.dat &> /root/log/fdt12.log &
-#fdtClient -c 10.20.3.101 -p 54313 -d /bulk/ssd13/  /bulk/ssd13/100Gfile.dat &> /root/log/fdt13.log &
-#fdtClient -c 10.20.3.101 -p 54314 -d /bulk/ssd14/  /bulk/ssd14/100Gfile.dat &> /root/log/fdt14.log &
-#fdtClient -c 10.20.3.101 -p 54315 -d /bulk/ssd15/  /bulk/ssd15/100Gfile.dat &> /root/log/fdt15.log &
+fdtClient -c 10.20.3.101 -p 54309 -d /bulk/ssd9/  /bulk/ssd9/100Gfile.dat &> /root/log/fdt9.log &
+fdtClient -c 10.20.3.101 -p 54310 -d /bulk/ssd10/  /bulk/ssd10/100Gfile.dat &> /root/log/fdt10.log &
+fdtClient -c 10.20.3.101 -p 54311 -d /bulk/ssd11/  /bulk/ssd11/100Gfile.dat &> /root/log/fdt11.log &
+fdtClient -c 10.20.3.101 -p 54312 -d /bulk/ssd12/  /bulk/ssd12/100Gfile.dat &> /root/log/fdt12.log &
+fdtClient -c 10.20.3.101 -p 54313 -d /bulk/ssd13/  /bulk/ssd13/100Gfile.dat &> /root/log/fdt13.log &
+fdtClient -c 10.20.3.101 -p 54314 -d /bulk/ssd14/  /bulk/ssd14/100Gfile.dat &> /root/log/fdt14.log &
+fdtClient -c 10.20.3.101 -p 54315 -d /bulk/ssd15/  /bulk/ssd15/100Gfile.dat &> /root/log/fdt15.log &
---
+++ initiate_fdt_100_single_server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+fdtClient -c 10.20.3.101 -d /bulk/ssd1/  /bulk/ssd1/100Gfile.dat &> /root/log/fdt1.log &
+fdtClient -c 10.20.3.101 -d /bulk/ssd2/  /bulk/ssd2/100Gfile.dat &> /root/log/fdt2.log &
+fdtClient -c 10.20.3.101 -d /bulk/ssd3/  /bulk/ssd3/100Gfile.dat &> /root/log/fdt3.log &
+fdtClient -c 10.20.3.101 -d /bulk/ssd4/  /bulk/ssd4/100Gfile.dat &> /root/log/fdt4.log &
+fdtClient -c 10.20.3.101 -d /bulk/ssd5/  /bulk/ssd5/100Gfile.dat &> /root/log/fdt5.log &
+fdtClient -c 10.20.3.101 -d /bulk/ssd6/  /bulk/ssd6/100Gfile.dat &> /root/log/fdt6.log &
+fdtClient -c 10.20.3.101 -d /bulk/ssd7/  /bulk/ssd7/100Gfile.dat &> /root/log/fdt7.log &
+fdtClient -c 10.20.3.101 -d /bulk/ssd8/  /bulk/ssd8/100Gfile.dat &> /root/log/fdt8.log &
+fdtClient -c 10.20.3.101 -d /bulk/ssd9/  /bulk/ssd9/100Gfile.dat &> /root/log/fdt9.log &
+fdtClient -c 10.20.3.101 -d /bulk/ssd10/  /bulk/ssd10/100Gfile.dat &> /root/log/fdt10.log &
+fdtClient -c 10.20.3.101 -d /bulk/ssd11/  /bulk/ssd11/100Gfile.dat &> /root/log/fdt11.log &
+fdtClient -c 10.20.3.101 -d /bulk/ssd12/  /bulk/ssd12/100Gfile.dat &> /root/log/fdt12.log &
+fdtClient -c 10.20.3.101 -d /bulk/ssd13/  /bulk/ssd13/100Gfile.dat &> /root/log/fdt13.log &
+fdtClient -c 10.20.3.101 -d /bulk/ssd14/  /bulk/ssd14/100Gfile.dat &> /root/log/fdt14.log &
+fdtClient -c 10.20.3.101 -d /bulk/ssd15/  /bulk/ssd15/100Gfile.dat &> /root/log/fdt15.log &
